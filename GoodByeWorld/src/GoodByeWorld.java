4e476dcf07e7d7db248f4efdafb14bc14b56062e
ctedwards <ctedwards@BNE-1R72F53.wvu-ad.wvu.edu>
2023-08-24 12:32:58 -0400

    i added GoodByeWorld project to the repo.

---
+++ GoodByeWorld/src/GoodByeWorld.java
@@ -0,0 +1,17 @@
+/** Author: Collin Edwards
+ * Date: 08/24/2023
+ * This is just a trial file
+ * This program prints out the name of my pets.\
+ * 
+ */
+public class GoodByeWorld {
+	public static void main(String[] args) {
+		// Print out a message to users
+		System.out.println("This is a message to display to users");
+		
+		// Print out an other message to users
+		System.out.println("This is a second message to display to users");
+
+	}
+
+}
