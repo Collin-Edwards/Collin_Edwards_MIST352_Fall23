e4ed7ab4b501590779cc4eb1e7af915ed323cc4a
collinedwards <collinedwards@localhost>
2023-09-01 15:34:03 -0400

    hopefully this pushes correctly

---
+++ In_Class_Activities/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,24 @@
+/** This is my in class activity that will hopefully export to my repo
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+System.out.println("Welcome to MIS352");
+	}
+
+}
